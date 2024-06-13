bd76e11bcf91152a611cc2c41bb87121d947ea19
Yeahn Kim <yeahnkim@gmail.com>
2024-06-13 12:19:13 -0700

    changed distance calculation method

--- project2.py
+++ project2.py
@@ -3,6 +3,8 @@ import sys
 import math
 from scipy.stats import zscore
 import scipy.stats
+import time
+import numpy as np
 
 def main():
     # print initial info. and receive filename and 
@@ -14,24 +16,21 @@ def main():
     print("\t2) Backward Elimination\n")
     selection = int(input())
 
-    # read data
-    data = pd.read_csv(filename, header=None, sep='  ', engine='python')
-
-    # do z-normalization
-    for col in data.columns:
-        if col==0:
-            continue
-        data[col] = (data[col]-data[col].mean())/data[col].std(ddof=0)
+    # read data & normalize it
+    data = get_data(filename)
+    # print(data[0]) # row
+    # print(data[:, 2]) # column
 
     # overall info
-    print(f"This dataset has {len(data.loc[0])-1} features (not including the class attribute), with {len(data)} instances.\n")
+    print(f"This dataset has {len(data[0])-1} features (not including the class attribute), with {len(data)} instances.\n")
 
     # Do a nearest neighbor with all features
-    print(f"""Running nearest neighbor with all {len(data.loc[0])-1} features, using "leaving-one-out" evaluation, I get an accuracy of {nearest_neighbor(data, [x for x in range(1, len(data.loc[0]))]):.1f}%""")
+    full_acc = nearest_neighbor(data, [x for x in range(1, len(data[0]))])
+    print(f"""Running nearest neighbor with all {len(data[0])-1} features, using "leaving-one-out" evaluation, I get an accuracy of {full_acc:1f}%""")
 
     # according to the choice, do leaving-one-out evaluation
     if selection == 1:
-        features = [[x] for x in range(1, len(data.loc[0]))]
+        features = [[x] for x in range(1, len(data[0]))]
     else:
         features = [[x for x in range(1, len(data.loc[0]))]]
     
@@ -42,10 +41,14 @@ def main():
     local_max_acc = -1
     local_max_set = []
     index = 0
+    start_time = time.time()
     while(True):
         feat = features[index]
         index+=1
-        acc = nearest_neighbor(data, feat)
+        if len(feat) == len(data[0])-1:
+            acc = full_acc
+        else:
+            acc = nearest_neighbor(data, feat)
         print(f"\t\tUsing feature(s) {set_to_string(feat)} accuracy is {acc:.1f}%")
         if acc > local_max_acc:
             local_max_acc = acc
@@ -65,10 +68,10 @@ def main():
             # check if we checked all feature sets
             # update features
             if selection==1:
-                if len(features[0]) == len(data.loc[0])-1:
+                if len(features[0]) == len(data[0])-1:
                     break
                 features = []
-                for i in range(1, len(data.loc[0])):
+                for i in range(1, len(data[0])):
                     if i not in local_max_set:
                         features.append(local_max_set+[i])
             else:
@@ -84,30 +87,41 @@ def main():
             local_max_acc = -1
             local_max_set = []
             index=0
-    
+    end_time = time.time()
     print(f"Finished search!! The best feature subset is {set_to_string(max_set)}, which has an accuracy of {max_accuracy:.1f}%")
+    print(f"This took: {end_time-start_time} seconds")
 
 def nearest_neighbor(data, feature):
     correct_count=0
-    for test_index, test_row in data.iterrows():
-        # print(f"\n=== testing row {test_index} ===")
+    distance_set = []
+
+    for test_index, test_row in enumerate(data):
+        distance_tmp = []
+        # print(f"=== testing row {test_index} ===")
         min_distance=sys.maxsize
         min_index=-1
-        for index, row in data.iterrows():
+        for index, row in enumerate(data):
             if test_index == index:
+                distance_tmp.append(0)
                 continue
-            distance = calc_distance(data, test_index, index, feature)
+            if test_index < index:
+                distance = calc_distance(data, test_index, index, feature)
+            else:
+                distance = distance_set[index][test_index]
+            distance_tmp.append(distance)
             # print(f"{index} - {distance}")
 
             # update nearest neighbor
             if distance < min_distance:
                 min_distance = distance
                 min_index = index
+        # print(distance_tmp)
+        distance_set.append(distance_tmp)
         
         # print(f"nearest neighbor is {min_index} - {min_distance}")
         
         # check correctness
-        if test_row[0] == data.loc[min_index][0]:
+        if test_row[0] == data[min_index][0]:
             # print(f"same class of {test_row[0]} == {data.loc[min_index][0]}")
             correct_count+=1
     # print(f"correct {correct_count} out of {len(data)}")
@@ -116,24 +130,26 @@ def nearest_neighbor(data, feature):
     return accuracy
 
 def calc_distance(data, t_index, index, feature_set):
-    test = data.loc[t_index]
-    row = data.loc[index]
-
-    test_feat = []
-    row_feat = []
-    for i in feature_set:
-        test_feat.append(test[i])
-        row_feat.append(row[i])
-    distance = math.dist(test_feat, row_feat)
+    test = data[t_index][feature_set]
+    row = data[index][feature_set]
+    distance = math.dist(test, row)
     return distance
+
+def get_data(filename):
+    data = pd.read_csv(filename, header=None, sep='  ', engine='python')
+
+    # do z-normalization
+    for col in data.columns:
+        if col==0:
+            continue
+        data[col] = (data[col]-data[col].mean())/data[col].std(ddof=0)
+    return data.to_numpy()
 
 def set_to_string(f_set):
     tmp = "{"
     for x in f_set:
         tmp += str(x) + ","
     return tmp[:-1] + "}"
-
-
 
 if __name__=="__main__":
     main()
