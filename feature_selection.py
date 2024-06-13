2e9cca9d056612bfa36a8fe4ced2a5dfe7c96e24
Yeahn Kim <yeahnkim@gmail.com>
2024-06-12 18:57:46 -0700

    accuracy testing

--- feature_selection.py
+++ feature_selection.py
@@ -19,16 +19,21 @@ def calc_distance(t_index, index, feature_set):
     #     print(row.loc[ind], end=', ')
     # print()
     distance = math.dist(test_feat, row_feat)
-    print(f"{index} : {distance}")
+    # print(f"{index} : {distance}")
     return distance
     # distance = math.dist(test)
 
+def set_to_string(f_set):
+    tmp = "{"
+    for x in f_set:
+        tmp += str(x) + ","
+    return tmp[:-1] + "}"
 
 
 # read data file
 # data = pd.read_csv('CS205_small_Data__27.txt', sep='  ', engine='python')
 # data = pd.read_csv('test1.txt', header=None, sep='  ', engine='python')
-data = pd.read_csv('test2.txt', header=None, sep='  ', engine='python')
+data = pd.read_csv('test3.txt', header=None, sep='  ', engine='python')
 # print(data.to_string())
 
 # print(len(data)) # row num
@@ -56,34 +61,70 @@ for _ in range(2, len(data.loc[0])):
                     features.append(tmp)
     previous = prev_tmp
 
-# print("features\n", features)
-# exit()
+
+print("Begin search.\n")
+
+feature_num=1
+max_feature_set=[]
+max_feature_local = []
+max_accuracy_set=0
+max_accuracy_local=0
 
 for cur_feature in features:
-correct_count=0
 
-for test_index, test_row in data.iterrows():
-    print(f"=== testing row {test_index} ===")
-    min_distance=sys.maxsize
-    min_index=-1
-    for index, row in data.iterrows():
-        if test_index==index:
-            continue
-        distance = calc_distance(test_index, index, 3)
+    # update features
+    if feature_num != len(cur_feature):
+        if max_accuracy_local > max_accuracy_set:
+            max_accuracy_set = max_accuracy_local
+            max_feature_set = max_feature_local
+        
+        print("\nFeature set ", end="")
+        print(set_to_string(max_feature_local), end="")
+        print(" was best, ", end='')
+        print(f"accuracy is {max_accuracy_local:.1f}%\n")
+        feature_num=len(cur_feature)
 
-        # update nearest neighbor
-        if distance < min_distance:
-            min_distance = distance
-            min_index = index
-    # check accuracy
-    print(f"nearest - {min_index} : {min_distance}")
-    if test_row[0] == data.loc[min_index][0]:
-        print(f"same class of {test_row[0]} == {data.loc[min_index][0]}")
-        correct_count+=1
-    print()
+        max_accuracy_local=0
+        max_feature_local=[]
+    
 
-print(f"correct {correct_count} out of {len(data)}")
-print(f"accuracy = {correct_count/len(data)*100}")
+    print("\t\tUsing feature(s) ", end="")
+    print(set_to_string(cur_feature), end="")
+    correct_count=0
+
+    for test_index, test_row in data.iterrows():
+        # print(f"=== testing row {test_index} ===")
+        min_distance=sys.maxsize
+        min_index=-1
+        for index, row in data.iterrows():
+            if test_index==index:
+                continue
+            distance = calc_distance(test_index, index, cur_feature)
+
+            # update nearest neighbor
+            if distance < min_distance:
+                min_distance = distance
+                min_index = index
+        # check accuracy
+        # print(f"nearest - {min_index} : {min_distance}")
+        if test_row[0] == data.loc[min_index][0]:
+            # print(f"same class of {test_row[0]} == {data.loc[min_index][0]}")
+            correct_count+=1
+        # print()
+
+    # print(f"correct {correct_count} out of {len(data)}")
+    accuracy = correct_count/len(data)*100
+    print(f" accuracy is {accuracy:.1f}%")
+    # print()
+
+    if accuracy > max_accuracy_local:
+        max_accuracy_local = accuracy
+        max_feature_local = cur_feature
+
+print("\nFinished search!! The best feature subset is ", end="")
+print(set_to_string(max_feature_set), end="")
+print(f", which has an accuracy of {max_accuracy_set:.1f}%")
+    
 
 # leaving-one-out evaluation (for all data)
     # find the nearest neighbor for that data
